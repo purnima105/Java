513c19b3d890a045a095617eefc9665a3b4e2266
purnima105 <gpurnima966@gmail.com>
2024-08-05 17:44:44 +0545

    changes made

--- CountingString.java
+++ CountingString.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class CountingString {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        String line = sc.nextLine();
+        
         
       
 
--- basic/series.java
+++ basic/series.java
@@ -1,4 +1,4 @@
-public class series
+public class Series
 {
 public static void main(String[]args)
 {
