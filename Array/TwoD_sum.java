6033f2bb0a38fa7cffc3b3b027394ea757be6021
purnima105 <gpurnima966@gmail.com>
2024-08-05 18:28:28 +0545

    adding

---
+++ Array/TwoD_sum.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.Scanner;
+public class TwoD_sum{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        int [][] num = new int [2][3];
+        int i,j;
+        int sum=0;
+        for(i=0;i<2;i++)
+        {
+            for (j=0;j<3;j++)
+            {
+                System.out.println("Enter the elememts of array: ");
+                num[i][j]= sc.nextInt();
+                sum=sum+num[i][j];
+            }
+        }
+        System.out.println("The elements entered by user are : ");
+        for(i=0;i<2;i++)
+        {
+            for (j=0;j<3;j++)
+            {
+                System.out.print("  "+num[i][j]);
+            }
+            System.out.println("\n");
+        }
+        System.out.println("The sum of elements entered by user = "+sum);
+    }
+}
