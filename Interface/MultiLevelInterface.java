daabefb6156929eb320b8da13248b532f9cca18b
purnima105 <gpurnima966@gmail.com>
2024-08-23 09:53:29 +0545

    adding

--- Interface/MultiLevelInterface.java
+++ Interface/MultiLevelInterface.java
@@ -1,12 +1,28 @@
 package Interface;
 interface i1
 {
-    public void add();
+     void add(int x,int y);
 }
-
-    
+interface i2 extends i1{
+     void div(int x,int y);
 }
-
+class Multi implements i2{
+    public void add(int x,int y)
+    {
+        System.out.println("adding the numbers we get: "+ (x+y));
+    }
+    public void div(int x,int y)
+    {
+        System.out.println("dividing the numbers we get: "+(x/y));
+    }
+}
 public class MultiLevelInterface {
+    public static void main(String[] args) {
+        Multi obj = new Multi();
+        obj.add(10, 5);
+        obj.div(10, 5);
+        
+
+    }
     
 }
