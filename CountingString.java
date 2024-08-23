95ef91d41c91a21a931500d7a9d100d630f2af63
purnima105 <gpurnima966@gmail.com>
2024-08-23 09:57:56 +0545

    adding

--- CountingString.java
+++ CountingString.java
@@ -1,12 +1,1 @@
 // wap in java to count the number of words in user inputted string
-
-import java.util.Scanner;
-
-public class CountingString {
-    public static void main(String[] args) {
-        
-        
-      
-
-    }
-}
