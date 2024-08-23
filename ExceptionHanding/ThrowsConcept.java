f00bbd645c1ad7a75fdcdbfe9ec17c5e66154746
purnima105 <gpurnima966@gmail.com>
2024-08-23 09:38:59 +0545

    adding

--- ExceptionHanding/ThrowsConcept.java
+++ ExceptionHanding/ThrowsConcept.java
@@ -1,15 +1,39 @@
 package ExceptionHanding;
 
-import java.io.FileNotFoundException;
 
-public class ThrowsConcept {
-    public void fun() throws ArrayIndexOutOfBoundsException,ArithmeticException,NullPointerException,FileNotFoundException
+
+class Throw {
+    public void fun() throws ArrayIndexOutOfBoundsException,ArithmeticException
     {
-        try
-        {
-            int a=
-
-        }
+       
+            int a=10;
+            int b=0;
+            System.out.println(a/b);
+            int [] n =new int [3];
+            n[3]=55;
+      
     }
-    
 }
+    public class ThrowsConcept{
+    public static void main(String[] args) {
+        Throw obj =new Throw();
+        try{
+            obj.fun();
+        }
+        
+        catch(ArithmeticException e)
+        {
+            System.out.println("Cannot be divided by zero");
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Invalid INDEX");
+        }
+        finally
+        {
+            System.out.println("Finally....");
+        }
+        System.out.println("HANDLEDD");   
+    }  
+}
+
